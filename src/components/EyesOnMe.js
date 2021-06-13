96c59d2d93e91f58c5d087387a6b890c9a597aad
Jhanae <jhanaechisholm12@gmail.com>
2021-06-13 13:09:05 -0400

    “Completed”

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+
+    function handleFocus(e){
+        console.log("Good!")
+    }
+    function handleBlurEvent(e){
+        console.log('Hey! Eyes on me!')
+    }
+    return(
+        <button onFocus={handleFocus} onBlur={handleBlurEvent}>Eyes on me</button>
+    )
+}
+export default EyesOnMe;
